e7a0cb95945f738bc5a52e68719b7b1d1e08adb0
L1mple <L1mple@rambler.ru>
2022-08-12 21:47:59 +0300

    debug=true

--- recruto/settings.py
+++ recruto/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 #SECRET_KEY = 'django-insecure-fwzxmaeh(%i13y)###qr$rm5j430+%5n++(u%&9_awc=w3#d+y'
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-fwzxmaeh(%i13y)###qr$rm5j430+%5n++(u%&9_awc=w3#d+y')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
